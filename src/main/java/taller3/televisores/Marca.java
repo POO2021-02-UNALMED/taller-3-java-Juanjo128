fcc6d4827070bfe9d4fbf48fa0ac9fd319ed6080
Juanjo128 <53496444+Juanjo128@users.noreply.github.com>
2021-11-02 16:10:19 -0500

    Create Marca.java

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,19 @@
+package taller3.televisores;
+
+public class Marca {
+	
+	String nombre;
+	
+	Marca(String x){
+		nombre = x;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String y) {
+		nombre = y;
+	}
+	
+}
